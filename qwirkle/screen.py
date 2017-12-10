b385314b6097f4b11766db78888187fc3e947ac1
jon-henningaasum <jon-henningaasum@10.0.0.117>
2017-12-10 21:42:33 +0100

    Learning to draw objects and get mouse inputs

--- qwirkle/screen.py
+++ qwirkle/screen.py
@@ -1,27 +1,53 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
-from nltk.app.nemo_app import images
 
 # Initialize the game
 pygame.init()
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Qwirkle')
+FPS = 30 #Frames per second setting
+fpsClock = pygame.time.Clock()
 
-#Load images
+#Mouse coordinates
+mousex = 0
+mousey = 0
+
+# set up the colors
+BLACK = ( 0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = ( 0, 255, 0)
+BLUE = ( 0, 0, 255)
+
+fontObj = pygame.font.Font('freesansbold.ttf', 32)
+textSurfaceObj = fontObj.render('Henrik!', True, GREEN, BLUE)
+textRectObj = textSurfaceObj.get_rect()
+textRectObj.center = (200, 150)
 
 #Game loop
-while 1:
-    #Clear the screen before drawing again
-    screen.fill(0)
-    #draw the screen elements
-    #screen.draw.rect()
-    #update the screen
-    pygame.display.flip()
+while True:
+    mouseClicked = False
     #loop through the events
     for event in pygame.event.get():
             #check if the event is the X button
             if event.type==pygame.QUIT:
                     pygame.quit()
                     exit(0)
+            elif event.type == MOUSEMOTION:
+                mousex, mousey = event.pos
+            elif event.type == MOUSEBUTTONUP:
+                mousex, mousey = event.pos
+                mouseClicked = True          
+    
+    #Draw on the screen
+    screen.fill(WHITE)
+    pygame.draw.rect(screen, RED, (200, 150, 100, 50))
+    pygame.draw.circle(screen, BLUE,  (mousex, mousey), 20, 0)
+    screen.blit(textSurfaceObj, textRectObj)
+    
+    #Update the screen
+    pygame.display.update()
+    fpsClock.tick(FPS)
     
     
